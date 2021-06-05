d5fd24df6ffc62045dfe54f58cd29ad8fc19a690
ChimpsterMan <emnilsson007@gmail.com>
2021-06-04 23:24:03 -0400

    set up node eviornment and began making the base html page and functions

---
+++ run_server.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 2000;
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/:File', function(req,res){
+    res.sendFile(__dirname + "/" + req.params.File);
+});
+
+io.on("connection", function (socket){
+    socket.on("newConnection", function(){
+        console.log("New Connection");
+    });
+});
+
+http.listen(port, function(){
+    console.log('listening on *:' + port);
+});
